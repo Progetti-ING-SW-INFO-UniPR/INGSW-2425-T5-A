0f7feb2b9251f326fc4c297a0f388477671ef8a6
MarcoAnghinetti <marco.anghinetti@studenti.unipr.it>
2025-06-05 17:45:54 +0200

    Pop-up Alert, funzione per crearli. Funzione TODO, per funnzionalità ancora non implementate

--- docker/webserver/AstroAllies/js/main.js
+++ docker/webserver/AstroAllies/js/main.js
@@ -13,3 +13,46 @@ function stopProp(el) {
 		ev.stopPropagation();
 	};
 }
+
+function createAlert(name, desc) {
+	// <div class="alert">
+	// 	<div>Name</div>
+	// 	<div>Description</div>
+	// </div>
+	let alertDiv = document.createElement("div");
+	let nameDiv = document.createElement("div");
+	let descDiv = document.createElement("div");
+	alertDiv.classList.add("alert");
+	nameDiv.appendChild(document.createTextNode(name));
+	descDiv.appendChild(document.createTextNode(desc));
+	alertDiv.appendChild(nameDiv);
+	alertDiv.appendChild(descDiv);
+	return alertDiv;
+}
+
+/**
+ * @param {string} name The name for the Alert, shows like a title
+ * @param {string} desc The description listed below the name
+ * @param {number} time The amout of seconds to show the allert for
+ * @param {string[]} classes Any amount of css-classes to give to the alert div element, like "err"
+ */
+function newAlert(name, desc, time=5, ...classes) {
+	let al = createAlert(name, desc);
+	for (c of classes) {
+		al.classList.add(c);
+	}
+	document.body.appendChild(al);
+	setTimeout(() => {
+		al.classList.add("al-show");
+		setTimeout(() => {
+			al.classList.remove("al-show");
+			setTimeout(() => {
+				document.removeChild(al);
+			}, 1000);
+		}, time*1000);
+	},1)
+}
+
+function TOOD() {
+	newAlert("Undefined Action", "The requested action is not yet available", 5, "err");
+}
