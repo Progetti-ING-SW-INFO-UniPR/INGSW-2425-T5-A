918ee50b86fc07c7aa1a59006d2efa22cbea8110
MarcoAnghinetti <marco.anghinetti@studenti.unipr.it>
2025-06-11 14:46:41 +0200

    Funzionamento registrazione

--- docker/webserver/AstroAllies/includes/credentialChecks.php
+++ docker/webserver/AstroAllies/includes/credentialChecks.php
@@ -1,13 +1,17 @@
 <?php
 function isValidUsername($username) : bool {
-	return preg_match("/^/w+$/", $username) == 1;
+	return preg_match("/^[A-Za-z0-9]+$/", $username) == 1;
+	// return true;
 }
 
 function isValidPassword($pwd) : bool {
 	return preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/", $pwd) == 1;
+	// return true;
 }
 
 function isValidEmail($email) : bool {
 	return preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$/", $email) == 1;
+	// return true;
+	//Aa12345!
 }
 ?>
--- docker/webserver/AstroAllies/pages/signup.php
+++ docker/webserver/AstroAllies/pages/signup.php
@@ -2,44 +2,6 @@
 session_start();
 require_once __DIR__ . "/./../includes/Database.php";
 require_once __DIR__ . "/./../includes/credentialChecks.php";
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-	$email = $_POST["email"];
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$sqlMail = "SELECT * FROM Utente WHERE Email = ?";
-	$sqlUser = "SELECT * FROM Utente WHERE Username = ?";
-	// $pepe = getenv('PSWD_PEPE');
-	$pepe = "2ry89^";
-	$sqlReg = "INSERT INTO Utente (Username, Email, Password) VALUES (?, ?, ?, ?)";
-
-	$db = new Database();
-
-	$result = $db->query($sqlMail, [$email]);
-	
-	if ($result && $result->num_rows > 0) {
-		echo "<script>newAlert('Email già in uso', 'L\'email fornita è già stata utilizzata per creare un account', 5, 'err')</script>";
-	} else {
-		$result = $db->query($sqlUser, [$username]);
-		if ($result && $result->num_rows > 0) {
-			echo "<script>newAlert('Nome Utente già in uso', 'Il Nome Utente fornito è già stato utilizzato per creare un account', 5, 'err')</script>";
-		} else {
-			if(isValidEmail($email) and isValidPassword($password) and isValidUsername($username)) {
-				$sale = random_bytes(8);
-				$result = $db->query($sqlReg, [$username, $email, password_hash($password+$pepe, PASSWORD_DEFAULT)]);
-			} else {
-				if(isValidEmail($email))
-					echo "<script>newAlert('Foramttazione errata', 'Email non riconosciuta', 5, 'err')</script>";
-				if(isValidUsername($username))
-					echo "<script>newAlert('Foramttazione errata', 'Il Nome Utente deve contenere solo lettere e cifre', 5, 'err')</script>";
-				if(isValidPassword($password))
-					echo "<script>newAlert('Foramttazione errata', 'La password deve avere: minimo 8 caratteri, una lettera maiuscoa, una minuscola, una cifra e un simbolo speciale', 5, 'err')</script>";
-			}
-		}
-	}
-
-	$db->close();
-}
 ?>
 <html lang="it">
 <head>
@@ -96,3 +58,47 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	<script src="../js/signup.js"></script>
 </footer>
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$email = $_POST["email"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$sqlMail = "SELECT * FROM Utente WHERE Email = ?;";
+	$sqlUser = "SELECT * FROM Utente WHERE Username = ?;";
+	// $pepe = getenv('PSWD_PEPE');
+	$pepe = "2ry89^";
+	$sqlReg = "INSERT INTO Utente (Username, Email, Password) VALUES (?, ?, ?)";
+	
+	$db = new Database();
+
+	$result = $db->query($sqlMail, [$email]);
+	
+	if ($result && $result->num_rows > 0) {
+		echo "<script>newAlert('Email già in uso', 'L\'email fornita è già stata utilizzata per creare un account', 5, 'err')</script>";
+	} else {
+		$result = $db->query($sqlUser, [$username]);
+		if ($result && $result->num_rows > 0) {
+			echo "<script>newAlert('Nome Utente già in uso', 'Il Nome Utente fornito è già stato utilizzato per creare un account', 5, 'err')</script>";
+		} else {
+			if(isValidEmail($email) and isValidPassword($password) and isValidUsername($username)) {
+				try {
+					$result = $db->query($sqlReg, [$username, $email, password_hash($password . $pepe, PASSWORD_DEFAULT)]);
+				} catch(Exception $err) {
+					$e = $err->getMessage();
+					// echo "<script>newAlert('Errore in data base', '" . $e . "', 5, 'err')</script>";
+					echo $e;
+				}
+			} else {
+				if(!isValidEmail($email))
+					echo "<script>newAlert('Foramttazione errata', 'Email non riconosciuta', 5, 'err')</script>";
+				if(!isValidUsername($username))
+					echo "<script>newAlert('Foramttazione errata', 'Il Nome Utente deve contenere solo lettere e cifre', 5, 'err')</script>";
+				if(!isValidPassword($password))
+					echo "<script>newAlert('Foramttazione errata', 'La password deve avere: minimo 8 caratteri, una lettera maiuscoa, una minuscola, una cifra e un simbolo speciale', 5, 'err')</script>";
+			}
+		}
+	}
+
+	$db->close();
+}
+?>
