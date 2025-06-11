4146ad3c3637b6f83f27cb0fd900a77626ffe0da
MarcoAnghinetti <marco.anghinetti@studenti.unipr.it>
2025-06-11 10:52:30 +0200

    Prima predisposizione per la pagina di gioco e funzioni necessarie

---
+++ docker/webserver/AstroAllies/js/game.js
@@ -0,0 +1,37 @@
+let g = document.getElementById("field");
+
+function drawShip(x, y, w, h, a) {
+
+}
+
+function drawAsteroid(x, y, w, h, r) {
+
+}
+
+function drawBullet(x, y, w, h, a) {
+
+}
+
+function drawDrop(x, y, w, h, type) {
+
+}
+
+function drawCannon(x, y, w, h, a) {
+
+}
+
+function drawScore() {
+
+}
+
+function drawEnergy() {
+
+}
+
+function drawComms() {
+
+}
+
+function drawPowerUps() {
+	
+}
