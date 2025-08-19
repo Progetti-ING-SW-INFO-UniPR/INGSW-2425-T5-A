abf16695c8de88bf0d3d862f6e730ccab8bed947
AndreaCagnolati <andrea.cagnolati@studenti.unipr.it>
2025-08-19 19:18:29 +0200

    copiate le classi di gioco

---
+++ docker/socketserver/app/includes/Box.php
@@ -0,0 +1,64 @@
+<?php
+class Box
+{
+    protected float $x;
+    protected float $y; // (x,y) spigolo in alto a sinistra della box
+    protected float $width;
+    protected float $height;
+
+    public function __construct(float $x,float $y,float $width,float $height){
+        $this->x = $x;
+        $this->y = $y;
+        $this->width = abs($width);
+        $this->height = abs($height);
+    }
+    public function deep_copy():Box{
+	    return unserialize(serialize($this));
+    }
+    public function get_x(): float{
+        return $this->x;
+    }
+    public function get_y(): float{
+        return $this->y;
+    }
+    public function get_width():float{
+        return $this->width;
+    }
+    public function get_height():float{
+        return $this->height;
+    }
+    public function set_x(float $x){
+        $this->x = $x;
+    }
+    public function set_y(float $y){
+        $this->y = $y;
+    }
+    public function set_width(float $width){
+        $this->width = abs($width);
+    }
+    public function set_heigth(float $height){
+        $this->height = abs($height);
+    }
+    public function __toString(): string{
+        $s = "X:" . $this->x ." Y:".$this->y." W:".$this->width." H:".$this->height;    
+        return $s;
+    }
+    public function check_overlap(Box $box): bool{
+        $x2 = $box->get_x();
+        $w2 = $box->get_width();
+        $y2 = $box->get_y();
+        $h2 = $box->get_height();
+        if( ($this->x + $this->width >= $x2 and $x2 + $w2 >= $this->x) and
+            ($this->y + $this->height >= $y2 and $y2 + $h2 >= $this->y)){
+            return true;
+        }
+        return false;
+    }
+
+    public function move(float $dx, float $dy){
+        $this->x += $dx;
+        $this->y += $dy;
+    }
+
+}
+?>
---
+++ docker/socketserver/app/includes/Spaceship.php
@@ -0,0 +1,154 @@
+<?php 
+require_once("Entity.php");
+require_once("Bullet.php");
+
+class Spaceship extends Entity{
+    protected int $energy; 
+    protected int $max_energy;
+    protected Bullet $ammo_type; // creare default bullet.
+    protected float $roto_dir;
+    protected float $roto_vel;
+    protected float $acceleration;
+    protected float $boost;
+
+    public function __construct(Vector $dir, Box $box, Bullet $b, int $e, float $ad, float $av, float $a, float $boost ,$g=null){
+        parent::__construct($dir,$box, $g);
+        $this->max_energy = $e;
+        $this->energy = $e;
+        $this->ammo_type = $b; 
+        $this->roto_dir = $ad;
+        $this->roto_vel = $av;
+        $this->acceleration = $a;
+        $this->boost = $boost;
+    }
+    function deep_copy():Spaceship{
+	    return unserialize(serialize($this));
+    }
+    public function get_max_energy():int {
+        return $this->max_energy;
+    }
+    public function set_max_energy(int $e){
+        $this->max_energy = $e;
+    }
+    public function get_energy(): int{
+        return $this->energy;
+    }
+    public function set_energy(int $e){
+        $this->energy = $e;
+    }
+    public function get_ammo(): Bullet{
+        return $this->ammo_type;
+    }
+    public function set_ammo(Bullet $b){
+        $this->ammo_type = $b;
+    }
+    public function get_roto_dir(): float{
+        return $this->roto_dir;
+    }
+    public function set_roto_dir(float $ad){
+        $this->roto_dir = $ad;
+    }
+    public function get_roto_vel(): float{
+        return $this->roto_vel;
+    }
+    public function set_roto_vel(int $av){
+        $this->roto_vel = $av;
+    }
+    public function get_acceleration(): float{
+        return $this->acceleration;
+    }
+    public function set_acceleration(float $a){
+        $this->acceleration = $a;
+    }
+    public function __toString():string{
+        $e = parent::__toString($this);
+        return $e . "MaxEnergy:" . $this->max_energy . "Energy:" . $this->energy . "Ammo:" . $this->ammo_type . "RDir:" . $this->roto_dir . "RVel:" . $this->roto_vel . "Acc:" . $this->acceleration;
+    }
+
+    /**
+     * Crea e aggiunge al relativo array un proiettile.
+     * 
+     * @param alfa angolo del cannone 
+     * 
+     * Se la navicella ha abbastanza energia ne consuma per
+     * creare e aggiungere al relativo array un proiettile @see add_bullet().
+     */
+    public function shoot(float $alfa){ //alfa è l'angolo del cannone
+        if($this->energy > 0){
+            $this->energy -= 1;
+            $dir = new Vector($alfa,$this->ammo_type->get_velocity()->get_norm());
+
+            $offset_x = ($this->hitbox->get_width() - $this->ammo_type->get_hitbox()->get_width())/2; //posizione proiettile centrata alla navicella
+            $offset_y = ($this->hitbox->get_height() - $this->ammo_type->get_hitbox()->get_height())/2;
+            $this->ammo_type->get_hitbox()->set_x($this->hitbox->get_x() + $offset_x);
+            $this->ammo_type->get_hitbox()->set_y($this->hitbox->get_y() + $offset_y);
+
+            $b = new Bullet($dir, clone $this->ammo_type->get_hitbox(), $this->ammo_type->get_rank());
+            $this->get_game()->add_bullet($b);
+        }
+    }
+    /**
+     * Aumenta la velocità della navicella. 
+     * 
+     * Se la navicella ha abbastanza energia ne consuma per
+     * aumentare la norma della velocità della navicella @see sum_norm().
+     */
+    public function boost(){
+        if($this->energy > 0){
+            $this->energy -= 1;
+            $this->velocity->sum_norm($this->boost);
+        }
+    }
+
+    /**
+     * Aggiorna la posizione della navicella
+     * 
+     * Somma alla velocità della navicella la sua accelerazione e l'attrito
+     * per impedire che la nacivella vada all'indietro in caso di somma negativa 
+     * forza la velocità a zero.
+     */
+    public function update(): void
+    {
+        $this->velocity->sum_norm($this->acceleration);
+        if($this->velocity->get_norm()-$this->game->get_friction() >= 0){
+            $this->velocity->sum_norm(-$this->game->get_friction());
+        } else {
+            $this->velocity->set_norm(0); 
+        }
+        parent::update();
+    }
+
+    /**
+     * Gestisce la reazione alla collisione 
+     * 
+     * @param e entità a cui reagire in base al tipo
+     * 
+     * In caso di collisione con Asteroid chiama @see game_over()
+     * Se collide con Item ne attiva l'effetto sulla navicella.
+     */
+    public function on_collision(Entity $e)
+    {
+        if(is_a($e, 'Asteroid'))
+            $this->game->game_over();
+        else if(is_a($e, 'Item')){
+            switch($e->get_type()){
+                case 2: // bullet vel upgrade
+                    $this->ammo_type->get_velocity()->sum_norm(5);
+                    break;
+                case 3: // bullet rank upgrade
+                    if($this->ammo_type->get_rank() < 3)
+                        $this->ammo_type->set_rank($this->ammo_type->get_rank()+1);
+                    break;
+                case 4: // bullet size upgrade
+                    if($this->ammo_type->get_hitbox()->get_width()+5 < $this->hitbox->get_width())
+                        $this->ammo_type->set_hitbox_comps(0,0,$this->ammo_type->get_hitbox()->get_width()+5,$this->ammo_type->get_hitbox()->get_height()+5);
+                    break;
+                default: // non fa nulla
+                    break;
+            }
+        }
+    }
+
+}
+
+?>
